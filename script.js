dd1a03a7b702b9359ce15a369883535f63a56398
Егор <58623077+SkornyakovEgor@users.noreply.github.com>
2021-08-08 16:24:54 +0500

    added a button to go to the top

--- script.js
+++ script.js
@@ -134,3 +134,28 @@ for (anchor of anchors) {
     })
   }
 }
+
+
+//---------------------------------------------------------------------------------------------------//
+
+
+let scrollToTopBtn = document.querySelector(".scroll-btn")
+let rootElement = document.documentElement
+
+function handleScroll() {
+  let scrollTotal = rootElement.scrollHeight - rootElement.clientHeight
+  if ((rootElement.scrollTop / scrollTotal ) > 0.10) {
+    scrollToTopBtn.classList.add("showBtn")
+  } else {
+    scrollToTopBtn.classList.remove("showBtn")
+  }
+}
+
+function scrollToTop() {
+  rootElement.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  })
+}
+scrollToTopBtn.addEventListener("click", scrollToTop)
+document.addEventListener("scroll", handleScroll)
