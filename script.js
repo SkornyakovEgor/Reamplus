8aafe62fdacbe7a8014419e47a0f3bfbb448d42e
Егор <58623077+SkornyakovEgor@users.noreply.github.com>
2021-08-08 12:26:59 +0500

    smooth scrolling to the anchor

--- script.js
+++ script.js
@@ -119,3 +119,18 @@ document.querySelector("#close-menu").onclick = function(){
 }
 
 //---------------------------------------------------------------------------------------------------//
+
+
+let anchors = document.querySelectorAll('header a[href*="#"]');
+
+for (anchor of anchors) {
+  if (anchor) {
+    anchor.addEventListener('click', function(e){
+      e.preventDefault();
+      anchorId = this.getAttribute('href');
+      document.querySelector(anchorId).scrollIntoView({
+        behavior: 'smooth', block: 'start'
+      })
+    })
+  }
+}
